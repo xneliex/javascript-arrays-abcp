4274bc6b4175d70ffe2ec243ba78b0baccd59324
Janeliz Sanders <clonegirl20@gmail.com>
2018-01-15 22:56:28 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -4,27 +4,3 @@ var chocolateBars = [
   "kitkat",
   "skittles"
 ];
-function addElementToBeginningOfArray (array, element){
-  return [array,...element];
-}
-function destructivelyAddElementToBeginningOfArray (array,element){
-  array.unshift(element);
-return array;
-}
-function addElementToEndOfArray (array,element){
-  return [...array, element];
-}
-function destructivelyAddElementToEndOfArray =function (array, element) {
-  array.push(element);
-  return array;
-}
-function accessElementInArray(array,index){
-  return array[index];
-}
-return array;
-function removeElementFromBeginningOfArray (array) {
-  return array.slice(1);
-}
-function removeElementFromEndOfArray (array){}
-  return array.slice(0, chocolateBars.lenght-1);
-}
