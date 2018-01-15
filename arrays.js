1f8077f9151395ca18ee91948e3907b19b486a3e
Janeliz Sanders <clonegirl20@gmail.com>
2018-01-15 23:10:08 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -4,7 +4,7 @@ var chocolateBars = [
   "kitkat",
   "skittles"
 ];
-function addElementToBeginningOfArray(array, element){
+function addElementToBeginningOfArray(chocolateBars, element){
 array.push("foo", 1)
 
 }
